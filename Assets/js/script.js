4e34d41a4d83290383991d7d83aa7dcf59c16774
Thanh Doan <doan.teddy@gmail.com>
2023-03-26 13:14:53 -0400

    finalizing changes

---
+++ Assets/js/script.js
@@ -0,0 +1,103 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function () {
+  let saveTime = 0;
+  let saveText = "";
+  let saveInput = JSON.parse(localStorage.getItem("userInput")) || [];
+
+  function init(){
+    setBlockColor();
+    setUserInput();
+    setTimeHeader();
+  }
+
+  init();
+
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  $(".saveBtn").click(function(){
+    saveTime = $(this).parent().attr('id').split("-")[1];
+    // brain exploded
+    saveText = $("textarea").get(saveTime - 9).value;
+    let userInput = {
+      time: saveTime,
+      note: saveText
+    };
+
+    saveInput = saveInput.filter(({ time }) => time !== saveTime);
+    saveInput.push(userInput);
+    localStorage.setItem("userInput", JSON.stringify(saveInput));
+
+  });
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  function setBlockColor(){
+    let currentTime = dayjs().format('HH');
+    console.log(currentTime);
+    let currentTimeElement = $(".time-block");
+    $(".time-block").each(function(index) {
+      if ((index+9) > currentTime)
+        $(this).attr("class", "row time-block future");
+      if ((index+9) < currentTime)
+      $(this).attr("class", "row time-block past");
+      if ((index+9) == currentTime)
+      $(this).attr("class", "row time-block present");
+    });
+
+    // for (let i = 9; i <= 17; i++){
+    //   if (currentTime > i){
+    //     let currentTimeElement = $(".time-block").get(i-9);
+
+    //     currentTimeElement[0].attr("class", "past");
+    //     currentTimeElement.setAtribute("class", "past");
+    //     .attr("class", "past") .get(i-9)
+    //   }
+    //   console.log($(".time-block").get(i-9).id.split("-")[1]);
+    // }
+    
+  }
+
+
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  function setUserInput(){
+    // TODO: try using $("TEXTAREA")
+    saveInput.forEach(function(entry){
+      $(".time-block").get(entry.time - 9).children[1].textContent = entry.note;
+
+      
+      // $(".time-block").get(entry.time - 9).text = entry.note;
+      // $(".time-block").get(entry.time - 9).children[1].val(entry.note) ;
+    })
+
+  }
+
+
+
+  // TODO: Add code to display the current date in the header of the page.
+  function setTimeHeader(){
+    var today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'); 
+    $('#currentDay').text(today);
+    
+    function updateTime(){
+      today = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a'); 
+      $('#currentDay').text(today);
+    }
+    
+    setInterval(updateTime, 1000);
+  }
+
+});
+
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
 
 ## Your Task
 
@@ -97,3 +97,70 @@ You are required to submit the following for review:
 
 - - -
 © 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+
+# CodeQuiz
+This is a quiz for nerds. All the questions are gaming related.
+
+## Description
+
+- I want to create a quiz game webapp that have questions about gaming
+- This project helps me better understand local storage, event listeners, DOM manipulations
+
+
+## Table of Contents 
+- [Usage](#usage)
+- [Links](#links)
+- [Features](#features)
+- [User Story](#user-story)
+- [Acceptance Criteria](#acceptance-criteria)
+
+
+## Usage
+
+![Screenshot](./assets/img/homescreen.png)
+
+![Screenshot](./assets/img/quizstart.png)
+
+![Screenshot](./assets/img/scorescreen.png)
+
+
+## Links
+
+- Github page: https://github.com/teddysm/CodeQuiz
+- Deployed page: https://teddysm.github.io/CodeQuiz/
+
+
+## Features
+
+- On the main page, user can start the game by clicking the start button, check the current scoreboard by clicking on the highscore button.
+- Once the quiz begins, the game cycles through a list of 12 questions, user can choose from 4 options
+- Clicking on the wrong answer will flash the screen red, right answer will flash green
+- The game ends when user runs out of time or completes all 12 questions
+- User can submit their initials to be saved to the Scoreboard (local storage)
+- Upon hitting submit, user will be directed to the HighScore page
+
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
